3354f60ac9c6ba4d6b248226567f95b9e7d6a4e0
KacperST <kstankiewicz@student.agh.edu.pl>
2026-03-18 01:09:33 +0100

    Add typing.

--- src/train/trainer.py
+++ src/train/trainer.py
@@ -1,8 +1,10 @@
+from collections.abc import Callable
 from models.bigram import BigramLanguageModel
 from torch.optim import AdamW
+import torch
 import torch.nn as nn
 
-def train_model(model: nn.Module, data, get_batch_fn: callable, max_iters, eval_fn, learning_rate: float= 1e-4):
+def train_model(model: nn.Module, data: torch.Tensor, get_batch_fn: Callable, max_iters: int, eval_fn: Callable, learning_rate: float = 1e-4) -> None:
     optimizer = AdamW(model.parameters(), lr=learning_rate)
     for step in range(max_iters):
         model.train()
