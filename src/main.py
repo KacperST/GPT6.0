734e3dc867e845921e5b30efb5b04ce8199dbfcf
KacperST <kstankiewicz@student.agh.edu.pl>
2026-03-19 00:27:10 +0100

    Add basic self attention and gpt model - to be continue

--- src/main.py
+++ src/main.py
@@ -7,13 +7,15 @@ from data.tokenizer import CharTokenizer
 from models.bigram import BigramLanguageModel
 from train.trainer import train_model
 from train.evaluate import evaluate_model
+from models.gpt import GPT
 
 BATCH_SIZE = 32
 BLOCK_SIZE = 8
-MAX_ITERS = 1000
+MAX_ITERS = 5000
 EVAL_INTERVAL = 300
 LEARNING_RATE = 1e-3
 MAX_NEW_TOKENS = 100
+EMBEDING_DIM = 32
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
 torch.manual_seed(1337)
@@ -24,7 +26,7 @@ def main():
     vocabulary = sorted(set(raw_data))
     tokenizer = CharTokenizer(vocabulary)
     data = tokenize_data(data=raw_data, tokenizer=tokenizer, to_tensor=True).to(DEVICE)
-    model = BigramLanguageModel(len(vocabulary))
+    model = GPT(len(vocabulary), EMBEDING_DIM, BLOCK_SIZE)
     model = model.to(device=DEVICE)
     train_model(model, data, get_batch, MAX_ITERS, evaluate_model, learning_rate=1e-3)
     idx_first = torch.zeros(1, 1, dtype=torch.long).to(DEVICE)
@@ -35,3 +37,25 @@ def main():
 
 if __name__ == "__main__":
     main()
+"""
+OUTPUT
+Step: 0, loss: 4.286160469055176
+Step: 500, loss: 3.2832372188568115
+Step: 1000, loss: 3.1075868606567383
+Step: 1500, loss: 2.955082416534424
+Step: 2000, loss: 2.749258041381836
+Step: 2500, loss: 2.6402783393859863
+Step: 3000, loss: 2.6137008666992188
+Step: 3500, loss: 2.571695566177368
+Step: 4000, loss: 2.5662286281585693
+Step: 4500, loss: 2.571314811706543
+Step: 4999, loss: 2.5537493228912354
+
+Generated sequence:
+SI:
+Thutour; pe thy, wresmyo ff h
+
+Aghe carveldro:
+An mof KVEEGOLE:
+We QSwhir, IOULroun qs imie wham
+"""
---
+++ src/models/gpt.py
@@ -0,0 +1,44 @@
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+from models.self_attention import SelfAttention
+
+
+class GPT(nn.Module):
+
+    def __init__(self, vocab_size: int, embed_dim: int, block_size: int):
+        super().__init__()
+        self.embeddings = nn.Embedding(vocab_size, embed_dim)
+        self.sa_head = SelfAttention(
+            embed_dim, head_size=embed_dim, block_size=block_size
+        )
+        self.positions = nn.Embedding(block_size, embed_dim)
+        self.lm_head = nn.Linear(embed_dim, vocab_size)
+        self.block_size = block_size
+
+    def forward(self, idx, target=None):
+        B, T = idx.shape
+        token_embeddings = self.embeddings(idx)  # B,T,C
+        position_embeddings = self.positions(torch.arange(0, T))  # T, C
+        x = token_embeddings + position_embeddings
+        x = self.sa_head(x)
+        logits = self.lm_head(x)
+        B, T, C = logits.shape
+
+        if target is None:
+            return logits, None
+
+        logits = logits.view(B * T, C)
+        target = target.view(B * T)
+        loss = F.cross_entropy(logits, target)
+        return logits, loss
+
+    def generate(self, idx: int, max_new_tokens: int = 500):
+        for _ in range(max_new_tokens):
+            idx_cond = idx[:, -self.block_size :]
+            logits, _ = self(idx_cond)
+            logits = logits[:, -1, :]
+            probs = F.softmax(logits, dim=-1)
+            idx_next = torch.multinomial(probs, num_samples=1)
+            idx = torch.cat([idx, idx_next], dim=1)
+        return idx
