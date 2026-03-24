76402df29652bb88afe8b256dabdbba821a991aa
KacperST <92333794+KacperST@users.noreply.github.com>
2026-03-24 11:53:19 +0000

    Add layer normalization

---
+++ src/models/normalization.py
@@ -0,0 +1,20 @@
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+
+DEVICE = 'cuda' if torch.cuda.is_available() else "cpu"
+
+class LayerNormalization(nn.Module):
+    
+    def __init__(self, embed_dim: int):
+        super().__init__()
+        self.g = nn.Parameter(torch.ones(embed_dim, device=DEVICE))
+        self.b = nn.Parameter(torch.zeros(embed_dim, device=DEVICE))
+        self.eps = 1e-5
+        
+
+    def forward(self, x):
+        layer_mean = torch.mean(x, dim=-1, keepdim=True)
+        std = torch.std(x, dim=-1, keepdim=True)
+        logits = (self.g / (std + self.eps)) * (x - layer_mean) + self.b
+        return logits
